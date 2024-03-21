c85636d614aea43f850a3a6ec295a5af1da6311e
Luis Martinez <A01748080@tec.mx>
2024-03-21 09:29:49 -0600

    Cambio de posicion comida

--- Juego_de_la_vibora/snake.py
+++ Juego_de_la_vibora/snake.py
@@ -1,14 +1,4 @@
-"""Snake, classic arcade game.
-
-Exercises
-
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to mouse clicks.
-"""
-
-from random import randrange
+from random import choice, randrange
 from turtle import *
 
 from freegames import square, vector
@@ -29,6 +19,16 @@ def inside(head):
     return -200 < head.x < 190 and -200 < head.y < 190
 
 
+def move_food():
+    """Move food randomly one step at a time."""
+    directions = [vector(10, 0), vector(-10, 0), vector(0, 10), vector(0, -10)]
+    direction = choice(directions)
+    new_food = food + direction
+
+    if inside(new_food):
+        food.move(direction)
+
+
 def move():
     """Move snake forward one segment."""
     head = snake[-1].copy()
@@ -41,17 +41,19 @@ def move():
 
     snake.append(head)
 
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9, 'black')
+
+    move_food()  # Move food randomly in each step
+
     if head == food:
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
         snake.pop(0)
-
-    clear()
-
-    for body in snake:
-        square(body.x, body.y, 9, 'black')
 
     square(food.x, food.y, 9, 'green')
     update()
@@ -66,5 +68,6 @@ onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
+move_food()  # Initialize food movement
 move()
 done()
